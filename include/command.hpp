3cf0ffbdd6373ef6ea2525bbfe030bd7b75985f7
Griger5 <griger04@gmail.com>
2025-02-15 19:14:17 +0100

    Add: Enum for argument types, updated Command with arg_types field and a new constructor

--- include/command.hpp
+++ include/command.hpp
@@ -8,6 +8,15 @@
 namespace skycli 
 {
 
+enum arg_type_t {
+    BOOL = 0,
+    INT = 1,
+    FLOAT = 2,
+    DOUBLE = 3,
+    CHAR = 4,
+    STRING = 5
+};
+
 struct BaseArgs {
     std::any _0;
     std::any _1;
@@ -22,15 +31,25 @@ class Command {
         std::function<void(BaseArgs *)> func;
         std::unique_ptr<BaseArgs> args;
         size_t arg_count;
+        std::vector<arg_type_t> arg_types;
 
     public:
         Command() {
             this->args = std::make_unique<BaseArgs>();
-            arg_count = 0;
+            this->arg_count = 0;
+        }
+
+        Command(std::function<void(BaseArgs *)> f, std::vector<arg_type_t> v) {
+            this->func = f;
+            this->args = std::make_unique<BaseArgs>();
+            this->arg_types = v;
+            this->arg_count = v.size();
         }
 
         Command &operator=(const Command& other) {
             this->func = other.func;
+            this->arg_count = other.arg_count;
+            this->arg_types = other.arg_types;
 
             for (size_t i = 0; i < this->arg_count; i++) {
                 this->get_arg(i) = other.get_arg(i);
