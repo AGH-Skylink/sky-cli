d6027b5b887bc7ec582b9cf90c16111d4540bb63
Griger5 <griger04@gmail.com>
2025-02-19 12:09:49 +0100

    Add: Static variable holder for the CLI class

---
+++ include/static_var_holder.hpp
@@ -0,0 +1,44 @@
+#ifndef SKYCLI_VAR_HOLDER
+#define SKYCLI_VAR_HOLDER
+
+#include "typing.hpp"
+
+#include <map>
+#include <string>
+#include <memory>
+
+namespace skycli {
+
+class StaticVarBase {};
+
+template <typename T>
+class StaticVar : public StaticVarBase {
+private:
+    T value;
+
+public:
+    StaticVar() = default;
+
+    T &get() {
+        return value;
+    }
+};
+
+class StaticVarHolder {
+private:
+    std::map<std::string, std::unique_ptr<StaticVarBase>> variables;
+
+public:
+    template <typename T>
+    void add_new_var(std::string var_name) {
+        variables[var_name] = std::make_unique<StaticVar<T>>();
+    }
+
+    StaticVarBase *get_ptr(std::string var_name) {
+        return variables[var_name].get();
+    }
+};
+
+} // namespace skycli
+
+#endif //SKYCLI_VAR_HOLDER
